ea00e0070e4fcb22f80f0389ac454d6f1fd231a8
anmol2520 <115863004+anmol2520@users.noreply.github.com>
2022-10-15 12:10:56 +0530

    Array Queue

---
+++ ArrayQueue.java
@@ -0,0 +1,105 @@
+package com.company;
+
+
+import java.util.Arrays;
+
+class Stack {
+    private int top;
+    private int[] stack;
+
+    public Stack(int length) {
+        stack = new int[length];
+        top = 0;
+    }
+
+    public void push(int element){
+        if(top >= stack.length)
+            throw new StackOverflowError();
+        stack[top] = element;
+        top++;
+    }
+
+    public int pop(){
+        if (top==0) {
+            System.out.println("StackUnder Flow");
+            return -1;
+        }
+        top--;
+        return stack[top];
+    }
+
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(stack);
+    }
+}
+
+class ArrayQueue {
+    private int[] queue;
+    private int front = 0;
+    private int rear = 0;
+    private int count = 0;
+
+    public ArrayQueue(int length) {
+        queue = new int[length];
+    }
+
+    public void enqueue(int element) {
+        if (count == queue.length)
+            throw new IllegalStateException();
+        queue[rear] = element;
+        rear = (rear + 1) % queue.length;
+        count++;
+    }
+
+    public void dequeue() {
+        if (count == 0)
+            throw new IllegalStateException();
+        var value = queue[front];
+        queue[front] = 0;
+        front = (front + 1) % queue.length;
+        count--;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(queue);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\nStack Operation");
+        Stack stack = new Stack(5);
+        System.out.println("Stack before Operations : " + stack);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        stack.pop();
+        stack.push(12);
+        System.out.println("Stack after Operations : " + stack);
+
+        System.out.println("\nQueue Operation");
+        ArrayQueue queue = new ArrayQueue(6);
+        System.out.println("Queue before Operations : " + queue);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(31);
+        queue.enqueue(32);
+        queue.enqueue(21);
+        queue.dequeue();
+        queue.dequeue();
+        System.out.println("Queue after Operations : " + queue);
+    }
+}
